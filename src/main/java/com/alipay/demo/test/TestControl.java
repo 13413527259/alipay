629303d40898de74ab7990c9bc5f70e2c4e20bce
陈俊华 <1109551489@qq.com>
2018-08-12 21:56:49 +0800

    0812
    1,扫码支付
    2.电脑网址支付
    3.账单下载
    （账单分，月账单/日账单，不能是今天或者今月）

--- src/main/java/com/alipay/demo/test/TestControl.java
+++ src/main/java/com/alipay/demo/test/TestControl.java
@@ -17,19 +17,28 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.alipay.api.AlipayApiException;
 import com.alipay.api.AlipayClient;
 import com.alipay.api.DefaultAlipayClient;
+import com.alipay.api.domain.AlipayDataDataserviceBillDownloadurlQueryModel;
 import com.alipay.api.domain.AlipayTradeCloseModel;
 import com.alipay.api.domain.AlipayTradeFastpayRefundQueryModel;
+import com.alipay.api.domain.AlipayTradePagePayModel;
+import com.alipay.api.domain.AlipayTradePrecreateModel;
 import com.alipay.api.domain.AlipayTradeQueryModel;
 import com.alipay.api.domain.AlipayTradeRefundModel;
 import com.alipay.api.domain.AlipayTradeWapPayModel;
 import com.alipay.api.internal.util.AlipaySignature;
+import com.alipay.api.request.AlipayDataDataserviceBillDownloadurlQueryRequest;
 import com.alipay.api.request.AlipayTradeCloseRequest;
 import com.alipay.api.request.AlipayTradeFastpayRefundQueryRequest;
+import com.alipay.api.request.AlipayTradePagePayRequest;
+import com.alipay.api.request.AlipayTradePrecreateRequest;
 import com.alipay.api.request.AlipayTradeQueryRequest;
 import com.alipay.api.request.AlipayTradeRefundRequest;
 import com.alipay.api.request.AlipayTradeWapPayRequest;
+import com.alipay.api.response.AlipayDataDataserviceBillDownloadurlQueryResponse;
 import com.alipay.api.response.AlipayTradeCloseResponse;
 import com.alipay.api.response.AlipayTradeFastpayRefundQueryResponse;
+import com.alipay.api.response.AlipayTradePagePayResponse;
+import com.alipay.api.response.AlipayTradePrecreateResponse;
 import com.alipay.api.response.AlipayTradeQueryResponse;
 import com.alipay.api.response.AlipayTradeRefundResponse;
 
@@ -57,6 +66,15 @@ public class TestControl {
 	@Value("${alipay.return_url}")
 	private String return_url;
 
+	/**
+	 * 手机网站支付
+	 * @param title
+	 * @param amount
+	 * @param order
+	 * @param httpResponse
+	 * @throws ServletException
+	 * @throws IOException
+	 */
 	@GetMapping("/testpay")
 	public void testpay(@RequestParam("title") String title, @RequestParam("amount") String amount,
 			@RequestParam("order") String order, HttpServletResponse httpResponse)
@@ -85,6 +103,12 @@ public class TestControl {
 		httpResponse.getWriter().close();
 	}
 
+	/**
+	 * 异步通知
+	 * @param map
+	 * @return
+	 * @throws AlipayApiException
+	 */
 	@PostMapping("/notify")
 	@ResponseBody
 	public String notify(@RequestParam Map<String, String> map) throws AlipayApiException {
@@ -104,6 +128,11 @@ public class TestControl {
 		return "success";
 	}
 
+	/**
+	 * 同步跳转
+	 * @param map
+	 * @return
+	 */
 	@GetMapping("/sync")
 	@ResponseBody
 	public Map<String, Object> sync(@RequestParam Map<String, Object> map) {
@@ -115,6 +144,12 @@ public class TestControl {
 		return map;
 	}
 
+	/**
+	 * 统一订单查询
+	 * @param queryModel
+	 * @return
+	 * @throws AlipayApiException
+	 */
 	@GetMapping("/query")
 	@ResponseBody
 	public String query(AlipayTradeQueryModel queryModel) throws AlipayApiException {
@@ -127,6 +162,13 @@ public class TestControl {
 		return response.getBody();
 	}
 
+	/**
+	 * 统一关闭订单
+	 * 未支付前
+	 * @param closeModel
+	 * @return
+	 * @throws AlipayApiException
+	 */
 	@GetMapping("/close")
 	@ResponseBody
 	public String close(AlipayTradeCloseModel closeModel) throws AlipayApiException {
@@ -139,6 +181,12 @@ public class TestControl {
 		return response.getBody();
 	}
 
+	/**
+	 * 统一退款
+	 * @param refundModel
+	 * @return
+	 * @throws AlipayApiException
+	 */
 	@GetMapping("/refund")
 	@ResponseBody
 	public String refund(AlipayTradeRefundModel refundModel) throws AlipayApiException {
@@ -151,6 +199,12 @@ public class TestControl {
 		return response.getBody();
 	}
 
+	/**
+	 * 统一退款查询
+	 * @param refundQueryModel
+	 * @return
+	 * @throws AlipayApiException
+	 */
 	@GetMapping("/refund/query")
 	@ResponseBody
 	public String refundQuery(AlipayTradeFastpayRefundQueryModel refundQueryModel) throws AlipayApiException {
@@ -161,6 +215,75 @@ public class TestControl {
 		request.setBizModel(refundQueryModel);
 		AlipayTradeFastpayRefundQueryResponse response = alipayClient.execute(request);
 		return response.getBody();
+	}
+	/**
+	 * 扫码支付
+	 * @param precreateModel
+	 * @param resp
+	 * @return
+	 * @throws AlipayApiException
+	 * @throws IOException
+	 */
+	@GetMapping("/scanpay")
+	public String scanPay(AlipayTradePrecreateModel precreateModel,HttpServletResponse resp) throws AlipayApiException, IOException {
+		// 获得初始化的AlipayClient
+		AlipayClient alipayClient = new DefaultAlipayClient(alipay_url, APP_ID, APP_PRIVATE_KEY, FORMAT, CHARSET,
+				ALIPAY_PUBLIC_KEY, SIGNTYPE);
+		AlipayTradePrecreateRequest request = new AlipayTradePrecreateRequest();
+		request.setBizModel(precreateModel);
+		AlipayTradePrecreateResponse response = alipayClient.execute(request);
+		resp.getWriter().write(response.getBody());
+		return null;
+	}
+	
+	/**
+	 * 统一账单下载
+	 * 使用的是沙箱测试，所以获取到的只是账单模板，想要下载获取账单数据，需使用正式环境测试； 
+	 * @param billModel
+	 * @param resp
+	 * @return
+	 * @throws AlipayApiException
+	 * @throws IOException
+	 */
+	@GetMapping("/bill")
+	@ResponseBody
+	public String bill(AlipayDataDataserviceBillDownloadurlQueryModel billModel,HttpServletResponse resp) throws AlipayApiException, IOException {
+		 //账单时间：日账单格式为yyyy-MM-dd，月账单格式为yyyy-MM。必须是前一天或者上一个月，不能当天当月
+		billModel.setBillDate("2018-08-09");
+		billModel.setBillType("signcustomer");
+		// 获得初始化的AlipayClient
+		AlipayClient alipayClient = new DefaultAlipayClient(alipay_url, APP_ID, APP_PRIVATE_KEY, FORMAT, CHARSET,
+				ALIPAY_PUBLIC_KEY, SIGNTYPE);
+		AlipayDataDataserviceBillDownloadurlQueryRequest   request = new AlipayDataDataserviceBillDownloadurlQueryRequest ();
+		request.setBizModel(billModel);
+		AlipayDataDataserviceBillDownloadurlQueryResponse response = alipayClient.execute(request);
+		resp.getWriter().write(response.getBody());
+		return null;
+	}
+	
+	/**
+	 * 电脑网站支付
+	 * @param billModel
+	 * @param resp
+	 * @return
+	 * @throws AlipayApiException
+	 * @throws IOException
+	 */
+	@GetMapping("/pagepay")
+	public void pagePay(AlipayTradePagePayModel pageModel,HttpServletResponse resp) throws AlipayApiException, IOException {
+		pageModel.setProductCode("FAST_INSTANT_TRADE_PAY");
+		// 获得初始化的AlipayClient
+		AlipayClient alipayClient = new DefaultAlipayClient(alipay_url, APP_ID, APP_PRIVATE_KEY, FORMAT, CHARSET,
+				ALIPAY_PUBLIC_KEY, SIGNTYPE);
+		AlipayTradePagePayRequest   request = new AlipayTradePagePayRequest ();
+		request.setNotifyUrl(notify_url);
+		request.setReturnUrl(return_url);
+		request.setBizModel(pageModel);
+		AlipayTradePagePayResponse response = alipayClient.pageExecute(request);
+		resp.setContentType("text/html;charset=" + CHARSET);
+		resp.getWriter().write(response.getBody());// 直接将完整的表单html输出到页面
+		resp.getWriter().flush();
+		resp.getWriter().close();
 	}
 
 }
